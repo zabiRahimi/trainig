c46428cabd332e378f0b2395f3ae490820e9daac
rza166 <esi191@yahoo.com>
2021-04-21 12:51:42 +0430

    1 ordibhesht

--- src/components/lessonsDeveloper/laravel/seeder.js
+++ src/components/lessonsDeveloper/laravel/seeder.js
@@ -20,6 +20,7 @@ const Seeder = () => {
               <button className='buttonA fontEn'  onClick={() => toChunkLesson('detailSeeder')}>معرفی سیدرها</button>
               <button className='buttonA fontEn' onClick={()=>toChunkLesson('createSeeder')}>ایجاد سیدر</button>
               <button className='buttonA fontEn' onClick={()=>toChunkLesson('editSeeder')}>آماده سازی و ویرایش seeder، تعداد رکوردها، استفاده از فکتوری</button>
+              <button className='buttonA fontEn' onClick={()=>toChunkLesson('callSeeder')}># فراخوانی seeder و اجرای دستور برای داده ریزی در دیتابیس</button>
 
            </div>
         </div>
@@ -64,6 +65,45 @@ const Seeder = () => {
         ->create();
     }`}
                 </pre>
+                <p className='fa'>
+                    با اجرای دستور بالا 3 رکورد ایجاد می شود. توسط متد <i className='enInPFa'>count()</i> تعداد رکوردهایی که می خواهیم ایجاد شود را مشخص می کنیم، همچنین می توانیم این عدد را مستقیم به متد <i className='enInPFa'>factory()</i> بدهیم، در این صورت نیازی به متد count نیست.
+                </p>
+
+            </div>
+            </div> {/* end .chunkLesson */}
+            
+            <div className='chunkLesson' id='callSeeder'>
+            <div className='titleLesson fa' ># فراخوانی seeder و اجرای دستور برای داده ریزی در دیتابیس </div>
+            <div className='articleLesson'>
+                <p className='fa'>
+                    برای اجرای یک seeder لازم است ابتدا seeder را در فایل databaseSeeder.php صدا زده شود. برای این منظور فایل <i className='enInPFa'>{`database > seeders > DatabaseSeeder.php`}</i> را باز کنید و متد run به صورت زیر ویرایش کنید
+                </p>
+                <pre className='en'>
+                    {`public function run()
+    {
+        $this->call([
+            UserSeeder::class,
+        ]);
+    }`}
+                </pre>
+                <p className='fa'>
+                    <i className='note'>نکته : در کد بالا در آرایه متد call سیدر UserSeeder را صدا زدیم دیگر سیدرها را نیز همین گونه به آرایه متد call اضافه کنید.</i>
+                </p>
+                <p className='fa'>
+                    بعد از فراخوانی  سیدرها در فایل DatabaseSeeder.php برای اجرای سیدرها از دستور آرتیزان زیر استفاده می کنیم
+                </p>
+                <pre className='en'>
+                    php artisan db:seed
+                </pre>
+                <p className='fa'>
+                    با اجرای دستور بالا تمام seeder های فراخوانی شده در فایل DatabaseSeeder.php اجرا می شوند. <br/> چنانچه بخواهیم فقط یک seeder اجرا شود به صورت زیر عمل می کنیم
+                </p>
+                <pre className='en'>
+                    php artisan db:seed --class=MySeeder
+                </pre>
+
+                
+
 
             </div>
             </div> {/* end .chunkLesson */}
